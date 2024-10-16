f9499396271ce539386e3ac2ba7df71991e92fc0
joo9810 <joo981001@kmu.kr>
2024-10-16 21:49:44 +0900

    10.16 study

---
+++ 빅데이터분석기사/part3/모의고사3/작업형1.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+
+df = pd.read_csv('Mental health.csv')
+# print(df)
+# print(df.info())
+
+# 1.
+mean_age = df['Age'].mean()
+# print(mean_age)
+df['Age'] = df['Age'].fillna(mean_age)
+# print(df['Age'])
+
+scaler = MinMaxScaler()
+df['Age'] = scaler.fit_transform(df[['Age']])
+
+print(len(df[df['Age'] >= 0.7]))
+
+
+
+# 2.
+hour_list = []
+for i in df['Timestamp']:
+    hour_list.append(i.split()[1])
+
+AMPM_list = []
+for i in hour_list:
+    if int(i.split(':')[0]) >= 12:
+        AMPM_list.append('PM')
+    else:
+        AMPM_list.append('AM')
+
+df['AM/PM'] = AMPM_list
+# print(df)
+
+AM_df = df[df['AM/PM'] == 'AM']
+# print(AM_df)
+print(len(AM_df[AM_df['specialist for a treatment'] == 'No']))
+
+# 3.
+label_encoder = LabelEncoder()
+
+# print(df)
+df['Marital status'] = label_encoder.fit_transform(df['Marital status'])
+df['Depression'] = label_encoder.fit_transform(df['Depression'])
+df['Anxiety'] = label_encoder.fit_transform(df['Anxiety'])
+df['Panic attack'] = label_encoder.fit_transform(df['Panic attack'])
+df['specialist for a treatment'] = label_encoder.fit_transform(df['specialist for a treatment'])
+# print(df) # Yes : 1, No : 0
+
+df['Total'] = df['Marital status'] + df['Depression'] + df['Anxiety'] + df['Panic attack'] + df['specialist for a treatment']
+# print(df)
+
+sorted_df = df.sort_values('Total', ascending=False).head(10)['current year of Study']
+print(sorted_df.sort_values(ascending=False).values[0])
