18353d88957f2d4a4717821a13a694415df2021a
joo9810 <joo981001@kmu.kr>
2025-01-10 01:30:48 +0900

    study

---
+++ 백준/자바/Main.java
@@ -0,0 +1,38 @@
+package 자바;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int room = 1;
+        int move = 0;
+
+        while (true) {
+            move += 1;
+            if (N <= room) {
+                break;
+            }
+            room += move*6;
+        }
+
+        System.out.println(move);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
