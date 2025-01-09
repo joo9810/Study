51ffda6439dc162441816a62d5dcd0a8453d9b2a
joo9810 <joo981001@kmu.kr>
2025-01-10 01:44:27 +0900

    study

--- 백준/자바/Main_1193.java
+++ 백준/자바/Main_1193.java
@@ -1,8 +1,10 @@
 package 자바;
 
+// 분수찾기
+
 import java.util.Scanner;
 
-public class Main {
+public class Main_1193 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
---
+++ 백준/자바/Main_2903.java
@@ -0,0 +1,20 @@
+package 자바;
+
+import java.util.Scanner;
+
+public class Main_2903 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int a = 2;
+        int result;
+    
+        for (int i=0; i<N; i++) {
+            a = a + (a-1);
+        }
+
+        result = (int) Math.pow(a, 2);
+    
+        System.out.println(result);
+    }
+}
