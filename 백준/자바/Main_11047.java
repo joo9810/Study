07e1413032949f3c5f44507e7fc8b358ace76e89
joo9810 <joo981001@kmu.kr>
2025-01-26 05:55:30 +0900

    study

---
+++ 백준/자바/Main_11047.java
@@ -0,0 +1,31 @@
+package 백준.자바;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken()); // 동전의 종류
+        int K = Integer.parseInt(st.nextToken()); // 만들어야 할 동전의 가치
+
+        int[] coinArr = new int[N]; // 동전의 종류를 담을 배열
+        for (int i=0; i<N; i++) {
+            coinArr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int remainValue = K;
+        int count = 0;
+        for (int i=coinArr.length-1; i>=0; i--) {
+            int currentCoin = coinArr[i];
+            if (remainValue >= currentCoin) {
+                count += remainValue / currentCoin;
+                remainValue %= currentCoin;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
---
+++ 백준/자바/Main_1764.java
@@ -0,0 +1,49 @@
+package 백준.자바;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_1764 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        // <String, Integer> 맵 생성
+        Map<String, Integer> map = new HashMap<>();
+        // 듣도 못한 사람 채우기
+        for (int i=0; i<N; i++) {
+            String noHear = br.readLine();
+            map.put(noHear, 1);
+        }
+
+        // 보도 못한 사람 채우기
+        for (int i=0; i<K; i++) {
+            String noSee = br.readLine();
+            if (map.containsKey(noSee)) {
+                map.put(noSee, map.get(noSee) + 1);
+            } else {
+                map.put(noSee, 1);
+            }
+        }
+
+        // 듣보잡 명단 담을 리스트
+        List<String> list = new LinkedList<>();
+        for (String name : map.keySet()) {
+            if (map.get(name) == 2) {
+                list.add(name);
+            }
+        }
+        // 리스트 정렬
+        Collections.sort(list);
+        
+        bw.write(list.size() + "\n");
+        for(String name : list) {
+            bw.write(name + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
